508c8017a1d5ee6641e45412512654000eae93c4
Abhishek Singh <47791443+thatdelhiboy@users.noreply.github.com>
2019-11-27 20:56:47 +0530

    update score fix

--- app.js
+++ app.js
@@ -1,92 +1,121 @@
-    const game = ()=> {
-        let pScore=0;
-        let cScore=0;
+        const game = ()=> {
+            let pScore=0;
+            let cScore=0;
 
-    //start the game
-        const startGame =()=>{
-            const playBtn = document.querySelector(".intro button");
-            const introScreen = document.querySelector(".intro");
-            const match = document.querySelector(".match");
+        //start the game
+            const startGame =()=>{
+                const playBtn = document.querySelector(".intro button");
+                const introScreen = document.querySelector(".intro");
+                const match = document.querySelector(".match");
 
-            //function for fadeout for after clicking play button
-        
-            playBtn.addEventListener('click', ()=> {
-                introScreen.classList.add("fadeOut");
-                match.classList.add("fadeIn");
-            });
-        };
-        //player Match
-        const playerMatch= () => {
-            const options = document.querySelectorAll(".options button");
-            const playerHand = document.querySelectorAll(".player-hand");
-            const computerHand = document.querySelectorAll(".computer-hand");
-            //options for computer
-
-            const computerOptions = ["rock", "paper", "scissors"];
-            options.forEach(option=>{
-                option.addEventListener("click", function(){
-                    //computer choices
-                    const computerNumber = Math.floor(Math.random() * 3 );
-                    const computerChoice = computerOptions[computerNumber];
-                    // here we will call compare hands
-                    compareHands(this.textContent, computerChoice);
-                    //update images
-                    playerHand.src =`./assets/${this.textContent}.png`;
-                    computerHand.src =`./assets/${computerChoice}.png`;
-                });
-            });
-        };
-        const   compareHands = (playerChoice, computerChoice)=> {
-            //udated text
-            const winner = document.querySelector('.winner');
-            //checking a tie
-            if (playerChoice === computerChoice){
-                winner.textContent == 'it is a tie';
-                return;
-            }
-            //check for rock
-            if (playerChoice === 'rock'){
-                if (computerChoice === 'scissors'){
-                    winner.textContent = 'player wins';
-                    return;
-                } else{
-                    winner.textContent = 'computer wins';
-                    return;
-                }
-            }
-            // check for scissors
-            if (playerChoice === 'scissors'){
-                if (computerChoice === 'paper'){
-                    winner.textContent = 'player wins';
-                    return;
-                } else{
-                    winner.textContent = 'computer wins';
-                    return;
-                }
-            }
-            // check for paper
-            if (playerChoice === 'paper'){
-                if (computerChoice === 'rock'){
-                    winner.textContent = 'player wins';
-                    return;
-                } else{
-                    winner.textContent = 'computer wins';
-                    return;
-                }
-            }
+                //function for fadeout for after clicking play button
             
-        }
+                playBtn.addEventListener('click', ()=> {
+                    introScreen.classList.add("fadeOut");
+                    match.classList.add("fadeIn");
+                });
+            };
+            //player Match
+            const playerMatch= () => {
+                const options = document.querySelectorAll(".options button");
+                const playerHand = document.querySelectorAll(".player-hand");
+                const computerHand = document.querySelectorAll(".computer-hand");
+                const hands = document.querySelectorAll(".hands img");
+                
+                hands.forEach(hand => {
+                    hand.addEventListener("animationend", function() {
+                      this.style.animation = "";
+                    });
+                  });
+                //Computer options
+                const computerOptions = ["rock", "paper", "scissors"];
+                
+                options.forEach(option=>{
+                    option.addEventListener("click", function(){
+                        //computer choices
+                        const computerNumber = Math.floor(Math.random() * 3 );
+                        const computerChoice = computerOptions[computerNumber];
+                        
+                        //here i have set timeous beause we want score to update at last.
+                        setTimeout(() => {
+                            //Here is where we call compare hands
+                            compareHands(this.textContent, computerChoice);
+                            //Update Images
+                            playerHand.src = `./assets/${this.textContent}.png`;
+                            computerHand.src = `./assets/${computerChoice}.png`;
+                          }, 2000);
+                          //Animation
+                            playerHand.style.animation = "shakePlayer 2s ease";
+                            computerHand.style.animation = "shakeComputer 2s ease";
+                    });
+                });
+            };
 
+            //updated score.
+            const updateScore = () =>{
+                const playerScore = document.querySelector('.player-score p');
+                const computerScore = document.querySelector('.computer-score p')
+                playerScore.textContent=pScore;
+                computerScore.textContent=cScore;
+            }
 
+            const   compareHands = (playerChoice, computerChoice)=> {
+                //udated text
+                const winner = document.querySelector('.winner');
+                //checking a tie
+                if (playerChoice === computerChoice){
+                    winner.textContent == 'it is a tie';
+                    return;
+                }
+                //check for rock
+                if (playerChoice === 'rock'){
+                    if (computerChoice === 'scissors'){
+                        winner.textContent = 'player wins';
+                        pScore++;
+                        updateScore();
+                        return;
+                    } else{
+                        winner.textContent = 'computer wins';
+                        cScore++;
+                        updateScore();
+                        return;
+                    }
+                }
+                // check for scissors
+                if (playerChoice === 'scissors'){
+                    if (computerChoice === 'paper'){
+                        winner.textContent = 'player wins';
+                        pScore++;
+                        updateScore();
+                        return;
+                    } else{
+                        winner.textContent = 'computer wins';
+                        cScore++;
+                        updateScore();
+                        return;
+                    }
+                }
+                // check for paper
+                if (playerChoice === 'paper'){
+                    if (computerChoice === 'rock'){
+                        winner.textContent = 'player wins';
+                        pScore++;
+                        updateScore();
+                        return;
+                    } else{
+                        winner.textContent = 'computer wins';
+                        cScore++;
+                        updateScore();
+                        return;
+                    }
+                }
+                
+            };
 
+            //here i called all the main functions
+            startGame();
+            playerMatch();
+        };
 
-
-
-
-        //here i called all the main functions
-        startGame();
-        playerMatch();
-    };
-
-    //here i called start game function
-    game();
+        //here i called start game function
+        game();
